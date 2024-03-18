5f1501e8dac03c5ede830f1da5ff07eef9c910ad
Larkin South <160358440+larkin-south@users.noreply.github.com>
2024-03-18 17:54:26 -0500

    03 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let textOutput = "";
 
+    for (let i = string.length - 1; i >= 0; i--) {
+        textOutput += string[i]
+    }
+
+    return textOutput;
 };
 
 // Do not edit below this line
