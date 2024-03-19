a3773e6cc6a22027c00b2a444b3612250ade1841
Larkin South <160358440+larkin-south@users.noreply.github.com>
2024-03-18 21:14:25 -0500

    Working on 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
 
+    let position;
+    let newArray = array;
+
+    while (array.indexOf(3) > -1) {
+        position = array.indexOf(3);
+        newArray.splice(position);
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
