c5d63aeb3bb0853ef355c524650dea13e307f8c3
Larkin South <160358440+larkin-south@users.noreply.github.com>
2024-03-20 17:09:13 -0500

    Working on functions for arrays.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,21 @@
 const removeFromArray = function(array,...args) {
     let newArray = [];
+    // for (item in args) {
+    //     for (value in array) {
+
+    //     }
+    // }
+
+    // return newArray;
+
     for (item in args) {
-        for (value in array) {
-
+            if (!array.includes(item)) {
+                let index = array.indexOf(item);
+                newArray.push(array.index);
+            }
         }
-    }
 
-    return newArray;
+    return array.indexOf(item);
 };
 
 // Do not edit below this line
