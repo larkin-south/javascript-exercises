3799a30b1b5fab936ab2633ca6792f5aa841e57f
Larkin South <160358440+larkin-south@users.noreply.github.com>
2024-03-21 18:06:04 -0500

    Working on 05.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,15 @@
-const sumAll = function(start, end) {
+const sumAll = function(num1, num2) {
+    let start = Math.min(num1, num2);
+    let end = Math.max(num1,num2);    
     let i = start + 1;
 
-    while (i <= end) {
-        start = start + i;
-        i++;
+    if (start < 0 || end < 0 || typeof end != 'number' || typeof start != 'number') {
+        start = "ERROR";
+    } else {
+        while (i <= end) {
+            start = start + i;
+            i++;
+        }
     }
 
     return start;
