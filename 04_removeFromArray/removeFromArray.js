9863f3c02b498dd824936d5b6d0d05084fb5dd7f
Larkin South <160358440+larkin-south@users.noreply.github.com>
2024-03-21 11:49:42 -0500

    Cleaned up commented code.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,21 +1,5 @@
 const removeFromArray = function(array,...args) {
     let newArray = [];
-    // for (item in args) {
-    //     for (value in array) {
-
-    //     }
-    // }
-
-    // return newArray;
-
-    // for (item in args) {
-    //         if (!array.includes(item)) {
-    //             let index = array.indexOf(item);
-    //             newArray.push(array.index);
-    //         }
-    //     }
-
-    // return array.indexOf(item);
 
     for (item in array) {
         if (!args.includes(array[item])) {
