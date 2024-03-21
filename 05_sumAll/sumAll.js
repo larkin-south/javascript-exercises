882546e38a2e902654d6c58e1b33641dfc6e5178
Larkin South <160358440+larkin-south@users.noreply.github.com>
2024-03-21 11:49:17 -0500

    Working on 05.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let i = start + 1;
 
+    while (i <= end) {
+        start = start + i;
+        i++;
+    }
+
+    return start;
 };
 
 // Do not edit below this line
