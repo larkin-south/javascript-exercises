aae2ef8c76904367274c7ec1da2aa03d79540b00
Larkin South <160358440+larkin-south@users.noreply.github.com>
2024-03-22 08:48:21 -0500

    06 complete.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  temp = (temp - 32) / 1.8;
+
+  if (temp != 0) {
+    temp = Number(temp.toFixed(1));
+  }
+
+  return temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  temp = (temp * 1.8) + 32;
+
+  if (temp != 0) {
+    temp = Number(temp.toFixed(1));
+  }
+
+  return temp;
 };
 
 // Do not edit below this line
