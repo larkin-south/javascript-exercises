f3f5fed254f7250d24186af4d8d48e49fa5b07a4
Larkin South <160358440+larkin-south@users.noreply.github.com>
2024-03-22 08:29:21 -0500

    05 complete.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,11 +3,11 @@ const sumAll = function(num1, num2) {
     let end = Math.max(num1,num2);    
     let i = start + 1;
 
-    if (start < 0 || end < 0 || typeof end != 'number' || typeof start != 'number') {
+    if (start < 0 || end < 0 || !Number.isInteger(num1) || !Number.isInteger(num2) || isNaN(start) || isNaN(end)) {
         start = "ERROR";
     } else {
         while (i <= end) {
-            start = start + i;
+            start += i;
             i++;
         }
     }
