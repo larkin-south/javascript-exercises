559d5249da6dd229ad7d0007cbe3a288e436619f
Larkin South <160358440+larkin-south@users.noreply.github.com>
2024-04-12 11:56:53 -0500

    08 and 09 complete.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((prev, current) => {return prev + current}, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((prev, current) => {return prev * current}, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(value) {
+  let total = value;
+  
+  if (value > 1) {
+    for (let i = value; (i - 1) > 0; i--) {
+      total *= (i - 1);
+    }
+  } else if (value == 0 || value == 1) { return 1 };
+
+  return total;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (value) {
+    let reg = RegExp(/[a-z,0-9]/i);
+    let word = [...value].filter(item => {
+        return reg.test(item) && item !== ",";
+    })
 
+    word = word.map(item => item.toLowerCase());
+
+    return word.join("") == word.reverse().join("") ? true : false;
 };
 
 // Do not edit below this line
