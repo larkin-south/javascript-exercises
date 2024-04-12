6c3ed095245b808bf88abbd7fb3b28abcb88560c
Larkin South <160358440+larkin-south@users.noreply.github.com>
2024-04-12 15:19:29 -0500

    10 complete.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(value) {
+    let array = [0,1];
+    
+    if (value < 0) { return "OOPS" };
+    
+    for (let i = 2; i <= value; i++) {
+        array.push(array[i-1] + array[i-2]);
+    }
 
+    return array[value];
 };
 
 // Do not edit below this line
