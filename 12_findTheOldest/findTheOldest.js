b277143f74ce1e77b3266eb4dc0a1c0e5afd4578
Larkin South <160358440+larkin-south@users.noreply.github.com>
2024-04-13 21:31:46 -0500

    12 complete.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    return array.reduce((prev, current) => {
+        let oldest = getAge(prev);
+        let newAge = getAge(current);
+
+        if (newAge > oldest) {
+            return current;
+        } else {
+            return prev;
+        }
+    })
+
 
 };
+
+function getAge(person) {
+    if (isNaN(person.yearOfDeath)) {
+        let currentYear = new Date().getFullYear();
+        let age = currentYear - person.yearOfBirth;
+        return age;
+    }
+    
+    let age = person.yearOfDeath - person.yearOfBirth;
+    return age;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
